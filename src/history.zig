73b59a0e793e7d8ec85666e58ebdbe285f523b90
jabolcni <jabolcni.strudelj@gmail.com>
2025-09-25 21:31:46 +0200

    modified:   src/history.zig

--- src/history.zig
+++ src/history.zig
@@ -46,8 +46,8 @@ pub fn update_corr_history(search: *Search, pos: *Position, corr_eval: i32, scor
 pub fn get_correction(search: *Search, pos: *Position) i32 {
     var corr_eval: i32 = 0;
     corr_eval += search.pawn_corr[pos.pawn_hash % CORRHIST_SIZE][pos.side_to_play.toU4()] * 4;
-    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[0] % CORRHIST_SIZE][pos.side_to_play.toU4()][0] * 16;
-    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[1] % CORRHIST_SIZE][pos.side_to_play.toU4()][1] * 16;
+    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[0] % CORRHIST_SIZE][pos.side_to_play.toU4()][0] * 4;
+    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[1] % CORRHIST_SIZE][pos.side_to_play.toU4()][1] * 4;
     //corr_eval += search.major_corr[pos.major_hash % CORRHIST_SIZE][pos.side_to_play.toU4()] * 4;
     //corr_eval += search.minor_corr[pos.minor_hash % CORRHIST_SIZE][pos.side_to_play.toU4()] * 4;
 
