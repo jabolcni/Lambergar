3e0ccae656df2129da61fe02f08654f99e7e61e4
jabolcni <jabolcni.strudelj@gmail.com>
2025-09-25 18:48:53 +0200

    modified:   src/history.zig

--- src/history.zig
+++ src/history.zig
@@ -39,15 +39,15 @@ pub fn update_corr_history(search: *Search, pos: *Position, corr_eval: i32, scor
     corr_update(&search.pawn_corr[pos.pawn_hash % CORRHIST_SIZE][pos.side_to_play.toU4()], err, weight);
     corr_update(&search.non_pawn_corr[pos.non_pawn_hash[0] % CORRHIST_SIZE][pos.side_to_play.toU4()][0], err, weight);
     corr_update(&search.non_pawn_corr[pos.non_pawn_hash[1] % CORRHIST_SIZE][pos.side_to_play.toU4()][1], err, weight);
-    corr_update(&search.major_corr[pos.major_hash % CORRHIST_SIZE][pos.side_to_play.toU4()], err, weight);
-    corr_update(&search.minor_corr[pos.minor_hash % CORRHIST_SIZE][pos.side_to_play.toU4()], err, weight);
+    //corr_update(&search.major_corr[pos.major_hash % CORRHIST_SIZE][pos.side_to_play.toU4()], err, weight);
+    //corr_update(&search.minor_corr[pos.minor_hash % CORRHIST_SIZE][pos.side_to_play.toU4()], err, weight);
 }
 
 pub fn get_correction(search: *Search, pos: *Position) i32 {
     var corr_eval: i32 = 0;
     corr_eval += search.pawn_corr[pos.pawn_hash % CORRHIST_SIZE][pos.side_to_play.toU4()] * 4;
-    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[0] % CORRHIST_SIZE][pos.side_to_play.toU4()][0] * 6;
-    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[1] % CORRHIST_SIZE][pos.side_to_play.toU4()][1] * 6;
+    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[0] % CORRHIST_SIZE][pos.side_to_play.toU4()][0] * 16;
+    corr_eval += search.non_pawn_corr[pos.non_pawn_hash[1] % CORRHIST_SIZE][pos.side_to_play.toU4()][1] * 16;
     //corr_eval += search.major_corr[pos.major_hash % CORRHIST_SIZE][pos.side_to_play.toU4()] * 4;
     //corr_eval += search.minor_corr[pos.minor_hash % CORRHIST_SIZE][pos.side_to_play.toU4()] * 4;
 
